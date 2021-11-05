988304e1d43a3722c3c54937da7163f68c8a1568
MatrixAge <xiewendao123@foxmail.com>
2021-11-05 17:48:20 +0800

    [add] 新增Cascader组件

---
+++ flows/cascader/index.input.js
@@ -0,0 +1,36 @@
+function main(args) {
+	return [
+		{
+			value: 'zhejiang',
+			label: 'Zhejiang',
+			children: [
+				{
+					value: 'hangzhou',
+					label: 'Hangzhou',
+					children: [
+						{
+							value: 'xihu',
+							label: 'West Lake'
+						}
+					]
+				}
+			]
+		},
+		{
+			value: 'jiangsu',
+			label: 'Jiangsu',
+			children: [
+				{
+					value: 'nanjing',
+					label: 'Nanjing',
+					children: [
+						{
+							value: 'zhonghuamen',
+							label: 'Zhong Hua Men'
+						}
+					]
+				}
+			]
+		}
+	]
+}
