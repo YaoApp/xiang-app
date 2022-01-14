3cb6a116d098676cf3b347a5209c9da528b4d75e
MatrixAge <xiewendao123@foxmail.com>
2022-01-14 15:24:25 +0800

    [add] ConditionForm

--- flows/find.indicator.js
+++ flows/find.indicator.js
@@ -1,5 +1,7 @@
 function main(args, out, res) {
-  var test = res["Test"];
-  test["ttt"] = 123;
-  return test;
+      var test = res['Test']
+      
+      test['survey_result'] = [70, [90, 70], 60, '2022-01-05', 'asdjajdjjdajdjasjd']
+      
+	return test
 }
