f2265248e4e9923f42aae5873d9d0d62a31f02f7
Wendao <xiewendao123@foxmail.com>
2022-02-25 18:01:34 +0800

    [add] DynamicList支持Cascader和treeSelect

--- flows/tree/index.input.js
+++ flows/tree/index.input.js
@@ -1,45 +1,45 @@
 function main(args) {
 	return [
 		{
-			title: '0-0',
-			key: '0-0',
+			title: 'A',
+			value: '0-0',
 			children: [
 				{
-					title: '0-0-0',
-					key: '0-0-0',
+					title: 'A-1',
+					value: '0-0-0',
 					children: [
-						{ title: '0-0-0-0', key: '0-0-0-0' },
-						{ title: '0-0-0-1', key: '0-0-0-1' },
-						{ title: '0-0-0-2', key: '0-0-0-2' }
+						{ title: 'A-1-1', value: '0-0-0-0' },
+						{ title: 'A-1-2', value: '0-0-0-1' },
+						{ title: 'A-1-3', value: '0-0-0-2' }
 					]
 				},
 				{
-					title: '0-0-1',
-					key: '0-0-1',
+					title: 'A-2',
+					value: '0-0-1',
 					children: [
-						{ title: '0-0-1-0', key: '0-0-1-0' },
-						{ title: '0-0-1-1', key: '0-0-1-1' },
-						{ title: '0-0-1-2', key: '0-0-1-2' }
+						{ title: 'A-2-1', value: '0-0-1-0' },
+						{ title: 'A-2-2', value: '0-0-1-1' },
+						{ title: 'A-2-3', value: '0-0-1-2' }
 					]
 				},
 				{
-					title: '0-0-2',
-					key: '0-0-2'
+					title: 'A-3',
+					value: '0-0-2'
 				}
 			]
 		},
 		{
-			title: '0-1',
-			key: '0-1',
+			title: 'B',
+			value: '0-1',
 			children: [
-				{ title: '0-1-0-0', key: '0-1-0-0' },
-				{ title: '0-1-0-1', key: '0-1-0-1' },
-				{ title: '0-1-0-2', key: '0-1-0-2' }
+				{ title: 'B-1', value: '0-1-0-0' },
+				{ title: 'B-2', value: '0-1-0-1' },
+				{ title: 'B-3', value: '0-1-0-2' }
 			]
 		},
 		{
-			title: '0-2',
-			key: '0-2'
+			title: 'C',
+			value: '0-2'
 		}
 	]
 }
